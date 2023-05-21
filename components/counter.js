a5df1cf7ddc0216e63586ee48aed63532590fb1c
unknown <aarati5737@gmail.com>
2023-05-21 11:48:28 +0545

    react-practice setcount created

--- components/counter.js
+++ components/counter.js
@@ -1,8 +1,16 @@
+'use client'
+import { useState } from "react"
+
 export default function Counter(){
+    const [count, setCount] = useState(0)
+    const handleClick = () => {
+        setCount(count + 1)
+        // count += 1
+    }
     return(
         <div>
-            <p></p>
-            <button>plus</button>
+            <p>{count}</p>
+            <button onClick={handleClick}>plus</button>
         </div>
     )
 }
